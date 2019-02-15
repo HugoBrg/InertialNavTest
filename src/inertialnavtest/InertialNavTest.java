9fc99b113b004ffd23d459bdcd80032b56ea7e08
HugoBrg <hugo.brg@outlook.com>
2019-02-15 02:43:37 +0100

    Début du projet

---
+++ src/inertialnavtest/InertialNavTest.java
@@ -0,0 +1,19 @@
+package inertialnavtest;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author hugob
+ */
+public class InertialNavTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        ExtractData ed1 = new ExtractData();
+    }
+    
+}
